845a17c9d081953a44588a21473f1c52a2e8d310
Anton Babenko <anton@antonbabenko.com>
2019-09-28 17:14:45 +0200

    Added creation of default VPC during bootstrap

--- bootstrap-workshop-jsonnet/vpc-with-defaults/vpc.tf
+++ bootstrap-workshop-jsonnet/vpc-with-defaults/vpc.tf
@@ -1,12 +1,18 @@
 locals {
-  cidr = "10.0.0.0/16"
+  cidr            = "10.0.0.0/16"
+  aws_cli_profile = "tfworkshop-admin"
 }
+
+data "aws_region" "selected" {}
 
 data "aws_availability_zones" "available" {}
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 2.0"
+
+  # Creation of VPC is disabled, because we need just default VPC created using CLI
+  create_vpc = false
 
   name = "default"
 
@@ -22,4 +28,10 @@ module "vpc" {
   assign_ipv6_address_on_creation = true
 
   public_subnet_ipv6_prefixes = [for k, v in data.aws_availability_zones.available.names : k]
+}
+
+resource "null_resource" "this" {
+  provisioner "local-exec" {
+    command = "aws ec2 create-default-vpc --region ${data.aws_region.selected.name} --profile ${local.aws_cli_profile}"
+  }
 }
